305228bfcdfe08a633e5a8939b270c90ad70bec6
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-04-05 18:21:27 -0500

    Create main.py, set up test dictionary
    Setting up a test dictionary to try and figure out how to draw names/values.

---
+++ main.py
@@ -0,0 +1,15 @@
+
+# Setting up a while loop for future use, but right now it would
+# just get in the way so it's commented out.
+# bidders = "y"
+# while bidders == "y":
+#   name = input("Please enter bidder's name: ")
+#   bidAmt = input("Please enter your bid amount: $")
+
+bidders = {
+    "Jay" : "10",
+    "Steve" : "35",
+    "Linus" : "176",
+    "Paul" : "62"
+
+}
